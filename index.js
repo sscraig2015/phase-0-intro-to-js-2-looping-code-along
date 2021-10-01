e445f7c0b7cced3823acafa61c5d8a3a11c91121
Sean <craig.sean92@gmail.com>
2021-09-30 19:02:25 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);*/
+
+const arr1 =[];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        
+        arr1.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+       
+    }
+    
+    return arr1;
+}
+function countDown (number) {
+    for (let i = 0; i <= number; i++) {
+        console.log(i);
+    }
+}
+
